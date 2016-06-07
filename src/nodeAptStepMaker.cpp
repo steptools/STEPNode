8a37b8a780244364ee2aa1a6d773bf0661c06d1f
Samson <bonfante@steptools.com>
2016-06-07 11:11:20 -0400

    Fixed formatting. We should be good from now on.

--- src/nodeAptStepMaker.cpp
+++ src/nodeAptStepMaker.cpp
@@ -21,39 +21,39 @@
 AptStepMaker* AptStepMaker::_singleton = nullptr;
 
 apt2step* AptStepMaker::getApt() {
-	if (_singleton == nullptr)
-		_singleton = new AptStepMaker();
-	return _singleton->_apt;
+    if (_singleton == nullptr)
+	_singleton = new AptStepMaker();
+    return _singleton->_apt;
 }
 
 NAN_METHOD(AptStepMaker::New)
 {
-	if (info.IsConstructCall())
+    if (info.IsConstructCall())
+    {
+	if (!info[0]->IsUndefined())
 	{
-		if (!info[0]->IsUndefined())
-		{
-			return;
-		}
-		if (_singleton == nullptr)
-			_singleton = new AptStepMaker();
-		_singleton->Wrap(info.This());
-		info.GetReturnValue().Set(info.This());
+	    return;
 	}
-	else
-	{
-		return;
-	}
+	if (_singleton == nullptr)
+	    _singleton = new AptStepMaker();
+	_singleton->Wrap(info.This());
+	info.GetReturnValue().Set(info.This());
+    }
+    else
+    {
+	return;
+    }
 }
 
 NAN_MODULE_INIT(AptStepMaker::Init)
 {
-	v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
-	tpl->SetClassName(Nan::New("AptStepMaker").ToLocalChecked());
-	tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+    tpl->SetClassName(Nan::New("AptStepMaker").ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-	Nan::SetPrototypeMethod(tpl, "OpenProject", OpenProject);
+    Nan::SetPrototypeMethod(tpl, "OpenProject", OpenProject);
 
-	Nan::SetPrototypeMethod(tpl, "GetToolNumber", GetToolNumber);
+    Nan::SetPrototypeMethod(tpl, "GetToolNumber", GetToolNumber);
 
     constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
     Nan::Set(target, Nan::New("AptStepMaker").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
@@ -62,34 +62,34 @@ NAN_MODULE_INIT(AptStepMaker::Init)
 
 NAN_METHOD(AptStepMaker::GetToolNumber)
 {
-	AptStepMaker * apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
-	if (!apt || !(apt->_apt)) return;
-	if (!info[0]->IsInt32())
-		return; // Throw error
+    AptStepMaker * apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
+    if (!apt || !(apt->_apt)) return;
+    if (!info[0]->IsInt32())
+	return; // Throw error
 
-	int id = info[0]->Int32Value();
-	const char * tlNum;
+    int id = info[0]->Int32Value();
+    const char * tlNum;
 
-	if (!apt->_apt->get_tool_number(id, tlNum))
-		return; // Throw error
+    if (!apt->_apt->get_tool_number(id, tlNum))
+	return; // Throw error
 
-	v8::Local<v8::Value> v8_tlNum;
-	size_t tlNum_len = v8StringToChar(v8_tlNum, &tlNum);
+    char* v8_tlNum;
+    //size_t tlNum_len = v8StringToChar(, tlNum);
 
-	return v8_tlNum;
+    //return v8_tlNum;
 }
 
 NAN_METHOD(AptStepMaker::OpenProject) {
-	AptStepMaker* apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
-	if (apt == 0) //Throw Exception
-		return;
-	if (info.Length() != 1) //Function should get one argument.
-		return;
-	if (!info[0]->IsString())
-		return;
-	char * fname = 0;
-	ssize_t fnamelen = v8StringToChar(info[0], fname);
-	if(!apt->_apt->read_238_file(fname)) //TODO: Handle Error.
-		return;
-	return; //Success finding, return.
+    AptStepMaker* apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
+    if (apt == 0) //Throw Exception
+	return;
+    if (info.Length() != 1) //Function should get one argument.
+	return;
+    if (!info[0]->IsString())
+	return;
+    char * fname = 0;
+    ssize_t fnamelen = v8StringToChar(info[0], fname);
+    if (!apt->_apt->read_238_file(fname)) //TODO: Handle Error.
+	return;
+    return; //Success finding, return.
 }
