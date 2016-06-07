54cb532982f1b001c4da8bb44083690d118e5b42
Roceso1337 <Roceso1337@gmail.com>
2016-06-07 16:08:23 -0400

    Finder::GetFaceEdgeNextPoint Added

--- src/nodeFinder.h
+++ src/nodeFinder.h
@@ -46,6 +46,19 @@ public:
     //long Finder::GetFaceEdgeCount(long face_id);
     static NAN_METHOD(GetFaceEdgeCount);
 
+    //void Finder::GetFaceEdgeNextPoint(
+    /*
+    System::Int64 face_id,
+    System::Int64 index,
+    [System::Runtime::InteropServices::Out] double %ret_x1,
+    [System::Runtime::InteropServices::Out] double %ret_y1,
+    [System::Runtime::InteropServices::Out] double %ret_z1,
+    [System::Runtime::InteropServices::Out] double %ret_x2,
+    [System::Runtime::InteropServices::Out] double %ret_y2,
+    [System::Runtime::InteropServices::Out] double %ret_z2
+    */ //);
+    static NAN_METHOD(GetFaceEdgeNextPoint);
+
     //string GetFeatureName(long id)
     static NAN_METHOD(GetFeatureName);
 
