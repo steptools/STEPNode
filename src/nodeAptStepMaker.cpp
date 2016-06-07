cf1e5a285d06c45c5ecd2cd5f5c79935527eecef
Will <will@wgrh.io>
2016-06-07 13:22:42 -0400

    Implemented AptStepMaker::GetToolNumber

--- src/nodeAptStepMaker.cpp
+++ src/nodeAptStepMaker.cpp
@@ -21,72 +21,91 @@
 AptStepMaker* AptStepMaker::_singleton = nullptr;
 
 apt2step* AptStepMaker::getApt() {
-    if (_singleton == nullptr)
-	_singleton = new AptStepMaker();
-    return _singleton->_apt;
+		if (_singleton == nullptr)
+				_singleton = new AptStepMaker();
+		return _singleton->_apt;
 }
 
 NAN_METHOD(AptStepMaker::New)
 {
-    if (info.IsConstructCall())
-    {
-	if (!info[0]->IsUndefined())
-	{
-	    return;
-	}
-	if (_singleton == nullptr)
-	    _singleton = new AptStepMaker();
-	_singleton->Wrap(info.This());
-	info.GetReturnValue().Set(info.This());
-    }
-    else
-    {
-	return;
-    }
+		if (info.IsConstructCall())
+		{
+				if (!info[0]->IsUndefined())
+				{
+						return;
+				}
+				if (_singleton == nullptr)
+						_singleton = new AptStepMaker();
+				_singleton->Wrap(info.This());
+				info.GetReturnValue().Set(info.This());
+		}
+		else
+		{
+				return;
+		}
 }
 
 NAN_MODULE_INIT(AptStepMaker::Init)
 {
-    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
-    tpl->SetClassName(Nan::New("AptStepMaker").ToLocalChecked());
-    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+		v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+		tpl->SetClassName(Nan::New("AptStepMaker").ToLocalChecked());
+		tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-    Nan::SetPrototypeMethod(tpl, "GetToolNumber", GetToolNumber);
-    Nan::SetPrototypeMethod(tpl, "OpenProject", OpenProject);
+		Nan::SetPrototypeMethod(tpl, "GetToolId", GetToolId);
+		Nan::SetPrototypeMethod(tpl, "GetToolNumber", GetToolNumber);
+		Nan::SetPrototypeMethod(tpl, "OpenProject", OpenProject);
 
-    constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
-    Nan::Set(target, Nan::New("AptStepMaker").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
+		constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
+		Nan::Set(target, Nan::New("AptStepMaker").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
 
+}
+
+NAN_METHOD(AptStepMaker::GetToolId)
+{
+		AptStepMaker * apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
+		if (apt == 0) //Throw exception
+				return;
+		if (info.Length() != 1) //Function should get one argument.
+				return;
+		if (!info[0]->IsString())
+				return;
+		char * tlNum = 0;
+		size_t tlNumLength = v8StringToChar(info[0], tlNum);
+		int tl_id;
+		if (!apt->_apt->get_tool_id(tlNum, tl_id)) //TODO: Handle error
+				return;
+		info.GetReturnValue().Set(tl_id);
+		return;
 }
 
 NAN_METHOD(AptStepMaker::GetToolNumber)
 {
-    AptStepMaker * apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
-    if (apt == 0) //Throw Exception
-	return;
-    if (info.Length() != 1) //Function should get one argument.
-	return;
-    if (!info[0]->IsInt32())
-	return;
-    int id = info[0]->Int32Value();
-    const char * tlNum;
-    if (!apt->_apt->get_tool_number(id, tlNum)) //TODO: Handle Error
-	return;
-    info.GetReturnValue().Set(CharTov8String((char *)tlNum));
-    return;
+		AptStepMaker * apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
+		if (apt == 0) //Throw Exception
+				return;
+		if (info.Length() != 1) //Function should get one argument.
+				return;
+		if (!info[0]->IsInt32())
+				return;
+		int id = info[0]->Int32Value();
+		const char * tlNum;
+		if (!apt->_apt->get_tool_number(id, tlNum)) //TODO: Handle Error
+				return;
+		info.GetReturnValue().Set(CharTov8String((char *)tlNum));
+		return;
 }
 
 NAN_METHOD(AptStepMaker::OpenProject) {
-    AptStepMaker* apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
-    if (apt == 0) //Throw Exception
-	return;
-    if (info.Length() != 1) //Function should get one argument.
-	return;
-    if (!info[0]->IsString())
-	return;
-    char * fname = 0;
-    ssize_t fnamelen = v8StringToChar(info[0], fname);
-    if (!apt->_apt->read_238_file(fname)) //TODO: Handle Error.
-	return;
-    return; //Success finding, return.
+		AptStepMaker* apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
+		if (apt == 0) //Throw Exception
+				return;
+		if (info.Length() != 1) //Function should get one argument.
+				return;
+		if (!info[0]->IsString())
+				return;
+		char * fname = 0;
+		ssize_t fnamelen = v8StringToChar(info[0], fname);
+		if (!apt->_apt->read_238_file(fname)) //TODO: Handle Error.
+				return;
+		return; //Success finding, return.
 }
