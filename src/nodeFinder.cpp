f7f718a70e5e8f1c08b0f3a4e984e4da4cc49ba8
nicholasfay <fayn@rpi.edu>
2016-06-07 13:07:45 -0400

    Added implementation for GetFeatureID

--- src/nodeFinder.cpp
+++ src/nodeFinder.cpp
@@ -104,3 +104,22 @@ NAN_METHOD(Finder::SaveAsP21)
 	if (!find->_find->save_file(file_name_utf8, false)) //Throw Exception
 		return;
 }
+
+NAN_METHOD(Finder::GetFeatureID){
+    Finder* find = Nan::ObjectWrap::Unwrap<Finder>(info.This()); 
+
+    if(info.Length() != 1)
+	return;
+    if(info[0]->IsUndefined())
+	return;
+    if (!info[0]->IsInt32())
+	return;
+
+    int feature_id = 0;
+
+    if (!find->_find->feature_id(info[0]->Int32Value(), feature_id))
+	return;
+
+    info.GetReturnValue().Set(feature_id);
+    return;	
+}
