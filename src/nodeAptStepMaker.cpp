52471ed6e113b3e0e685fcdad4094e0b06bc9889
Will <will@wgrh.io>
2016-06-07 12:14:32 -0400

    Fixed merge issues that resulted in multiple headers

--- src/nodeAptStepMaker.cpp
+++ src/nodeAptStepMaker.cpp
@@ -21,68 +21,6 @@
 AptStepMaker* AptStepMaker::_singleton = nullptr;
 
 apt2step* AptStepMaker::getApt() {
-    if (_singleton == nullptr)
-	_singleton = new AptStepMaker();
-    return _singleton->_apt;
-}
-
-NAN_METHOD(AptStepMaker::New)
-{
-    if (info.IsConstructCall())
-    {
-	if (!info[0]->IsUndefined())
-	{
-	    return;
-	}
-	if (_singleton == nullptr)
-	    _singleton = new AptStepMaker();
-	_singleton->Wrap(info.This());
-	info.GetReturnValue().Set(info.This());
-    }
-    else
-    {
-	return;
-    }
-}
-
-NAN_MODULE_INIT(AptStepMaker::Init)
-{
-    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
-    tpl->SetClassName(Nan::New("AptStepMaker").ToLocalChecked());
-    tpl->InstanceTemplate()->SetInternalFieldCount(1);
-
-    Nan::SetPrototypeMethod(tpl, "OpenProject", OpenProject);
-
-    Nan::SetPrototypeMethod(tpl, "GetToolNumber", GetToolNumber);
-
-    constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
-    Nan::Set(target, Nan::New("AptStepMaker").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
-
-}
-
-// $RCSfile: $
-// $Revision: $ $Date: $
-// Auth: Samson Bonfante (bonfante@steptools.com)
-// 
-// Copyright (c) 1991-2016 by STEP Tools Inc. 
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include "nodeAptStepMaker.h"
-#include "nodeUtils.h"
-AptStepMaker* AptStepMaker::_singleton = nullptr;
-
-apt2step* AptStepMaker::getApt() {
 	if (_singleton == nullptr)
 		_singleton = new AptStepMaker();
 	return _singleton->_apt;
@@ -125,16 +63,16 @@ NAN_MODULE_INIT(AptStepMaker::Init)
 NAN_METHOD(AptStepMaker::GetToolNumber)
 {
 	AptStepMaker * apt = Nan::ObjectWrap::Unwrap<AptStepMaker>(info.This());
-	if (!apt) return;
+	if (apt == 0) //Throw Exception
+		return;
+	if (info.Length() != 1) //Function should get one argument.
+		return;
 	if (!info[0]->IsInt32())
-		return; // Throw error
-
+		return; 
 	int id = info[0]->Int32Value();
 	const char * tlNum;
-
-	if (!apt->_apt->get_tool_number(id, tlNum))
-		return; // Throw error
-
+	if (!apt->_apt->get_tool_number(id, tlNum)) //TODO: Handle Error
+		return;
 	info.GetReturnValue().Set(CharTov8String((char *)tlNum));
 	return;
 }
