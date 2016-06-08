f9cfa2f18be6af9af647c6a033a1ba34cdc70df2
nicholasfay <fayn@rpi.edu>
2016-06-08 14:49:27 -0400

    Added Implementation for GetExecutableWorkpieceAsIs

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var finder = new StepNC.Finder();
 finder.OpenProject("model.stpnc");
 var a = 94989;
 
-console.log(finder.IsSelective(a));
-console.log(finder.IsWorkingStep(a));
-console.log(finder.IsWorkplan(a));
+console.log(finder.GetExecutableWorkpieceAsIs(a));
 
 process.exit();
