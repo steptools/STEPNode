53deae9081394f2c4550b2c5e6604697ef7ba614
Patrick Hesselbach <hessep@rpi.edu>
2016-06-22 16:12:35 -0400

    implemented GetToleranceAllCount/Next

--- src/nodeTolerance.cpp
+++ src/nodeTolerance.cpp
@@ -1,15 +1,15 @@
 // $RCSfile: $
 // $Revision: $ $Date: $
 // Auth: Samson Bonfante (bonfante@steptools.com)
-// 
-// Copyright (c) 1991-2016 by STEP Tools Inc. 
-// 
+//
+// Copyright (c) 1991-2016 by STEP Tools Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -45,8 +45,41 @@ NAN_MODULE_INIT(Tolerance::Init)
     tpl->SetClassName(Nan::New("Tolerance").ToLocalChecked());
     tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
+    Nan::SetPrototypeMethod(tpl, "GetToleranceAllCount", GetToleranceAllCount);
+    Nan::SetPrototypeMethod(tpl, "GetToleranceAllNext", GetToleranceAllNext);
+
     constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
     Nan::Set(target, Nan::New("Tolerance").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
 
 }
 
+NAN_METHOD(Tolerance::GetToleranceAllCount)
+{
+  Tolerance* tol = Nan::ObjectWrap::Unwrap<Tolerance>(info.This());
+  if(!tol)
+    return;
+  if(info.Length!=0)
+    return;
+  int size = 0;
+  if(!tol->_tol->tolerance_count(size))
+    return;
+  info.GetReturnValue().Set(size);
+  return;
+}
+
+NAN_METHOD(Tolerance::GetToleranceAllNext)
+{
+  Tolerance* tol = Nan::ObjectWrap::Unwrap<Tolerance>(info.This());
+  if(!tol)
+    return;
+  if(info.Length!=1)
+    return;
+  if(!info[0]->IsNumber())
+    return;
+  int index = (int)info[0];
+  int tol_id = 0;
+  if (!tol->_tol->tolerance_next(index, tol_id))
+    return;
+  info.GetReturnValue().Set(tol_id);
+  return;
+}
