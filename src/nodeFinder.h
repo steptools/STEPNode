bc193d7ee5fc4595c9317c6a934f762ac1e526ac
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2016-07-12 10:29:33 -0400

    Implemented GetWorkpieceImmediateSubAssemblyAll()

--- src/nodeFinder.h
+++ src/nodeFinder.h
@@ -256,6 +256,9 @@ public:
     //int GetWorkpieceAsIsOfMain()
     static NAN_METHOD(GetWorkpieceAsIsOfMain);
 
+    //int[] GetWorkpieceImmediateSubAssemblyAll
+    static NAN_METHOD(GetWorkpieceImmediateSubAssemblyAll);
+
     //int[] GetWorkpieceSubAssemblyAll
     static NAN_METHOD(GetWorkpieceSubAssemblyAll);
 
