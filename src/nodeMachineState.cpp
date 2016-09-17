afe115947e60a653aa0340b05667a16abd4c94e7
Samson <bonfante@steptools.com>
2016-09-17 16:31:07 -0400

    trying to fix handlescope errors

--- src/nodeMachineState.cpp
+++ src/nodeMachineState.cpp
@@ -67,7 +67,8 @@ NAN_METHOD(machineState::New)
 		auto myiso = v8::Isolate::New();
 		v8::Locker lock(myiso);
 		v8::Isolate::Scope isoscope(myiso);
-		v8::HandleScope handlescope(myiso);
+		v8::EscapableHandleScope handlescope(myiso);
+		myiso->Enter();
 		while (wait) {
 		    void * vpmise;
 		    wait = ms->_ms->WaitForStateUpdate(vpmise, rtn);
