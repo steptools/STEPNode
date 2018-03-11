7ccfced1ae7cf64daf9b74733eeae92dab8a4ed8
David Loffredo <loffredo@steptools.com>
2018-03-10 23:04:18 -0500

    copyright bump
    Signed-off-by: David Loffredo <loffredo@steptools.com>

--- examples/buildsimple.js
+++ examples/buildsimple.js
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 1991-2017 by STEP Tools Inc.
+ * Copyright (c) 1991-2018 by STEP Tools Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
